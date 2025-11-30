788740badf632eef3e7f440143789107d3cac6ba
towa <hoshi.no.natu@gmail.com>
2025-11-30 18:12:21 +0900

    Fix: Featureテスト全て成功（CSRF修正）

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+  use CreatesApplication;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+
+    // ← Laravel10/11 正しいCSRFミドルウェア
+    $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+  }
 }
